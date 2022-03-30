f9f43504fd8d37e9616131129d11fe9a51917f4f
Danilo Almeida <cs.daniloalmeida@gmail.com>
2022-03-29 22:11:37 -0300

    initial commit

---
+++ converter.py
@@ -0,0 +1,93 @@
+import os
+
+# Coluna em que os remedios comecam
+remedios_col = 46
+
+# Nome do arquivo .data a ser criado
+nome_data = os.path.join("data", "pmc.data")
+
+# ---------------------------------------------------------------------------------------
+# NAO MODIFICAR A PARTIR DAQUI
+
+# CONVERTE UM XML EM UM DICIONARIO PYTHON, QUE SERA GUARDADO UM ARQUIVO .DATA
+
+def remove_text(string):
+    return str(string).replace("text:", "").replace("'", "")
+
+def main():
+    import xlrd
+    import pickle
+    from sys import exit
+    from time import sleep
+
+    global nome_data
+    global nome_xls
+    global remedios_col
+
+    # Checar a existencia de um diretorio "data"
+    data_exists = False
+    for i in os.listdir():
+        if i == "data":
+            data_exists = True
+    
+    if not data_exists:
+        os.mkdir("data")
+
+    # Checar a existencia de um arquivo xls
+    print("Procurando arquivo .xls...")
+    xls_exists = False;
+    for file in os.listdir():
+        if (os.path.isfile(file) and file.endswith(".xls")):
+            xls_exists = True
+            nome_xls = file
+
+    if not xls_exists:
+        print("ERRO: Arquivo .xls nao encontrado.")
+        sleep(4)
+        return 1
+
+    # Criar arquivo data, caso ele nao exista ainda
+    if not os.path.exists(nome_data):
+        print("Criando arquivo .data...")
+        open(nome_data, "w")
+
+    print("Lendo .xls...")
+    # Carregar arquivo xls
+    wb = xlrd.open_workbook(nome_xls)
+    sh = wb.sheet_by_index(0)
+
+    # Subtrair 1 de remedios_col por conta de como indexes funcionam
+    remedios_col -= 1
+    categorias = []
+    remedios = {}
+
+    # Popular categorias que todo remedio vai possuir
+    # e.g: nome, produto, pmc, etc
+    for i in sh.row(remedios_col - 1):
+        categorias.append(remove_text(i))
+
+    print("Convertendo dados...")
+    for fileira in range(remedios_col, sh.nrows):
+        # Criar cada remedio como um value do dicionario "remedios" e atribuir
+        # um dicionario vazio a ele, que possuira suas categorias
+        remedios[fileira] = {}
+        for categoria in range(len(categorias)):
+            # Atribuir categorias a cada remedio
+            remedios[fileira][categorias[categoria]] = remove_text(sh.row(fileira)[categoria])
+
+    # Organizar remedios por ordem alfabetica do produto
+    print("Organizando remedios...")
+    remedios = dict(sorted(tuple(remedios.items()), key=lambda x: x[1]['PRODUTO']))
+
+    print("Escrevendo dados...")
+    with open(nome_data, "wb") as wf:
+        pickle.dump(remedios, wf)
+
+    print("Convertido com sucesso!")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nProcesso interrompido.\nSaindo...")
+        exit(0)
---
+++ pesquisar.py
@@ -0,0 +1,52 @@
+from os import system, name
+
+def main():
+    import pickle
+    import os
+    from sys import exit
+    from converter import nome_data
+    from time import sleep
+
+    if not os.path.exists(nome_data):
+        print("Arquivo .data nao encontrado. Execute \"converter.py\" primeiro.")
+        sleep(4)
+        return 1;
+    
+    with open(nome_data, "rb") as rf:
+        remedios = pickle.load(rf)
+
+    nome = ""
+    while True:
+        resultados = []
+        print("-"*80)
+        nome = input("Remedio: ").strip().upper()
+        system('cls' if name == 'nt' else 'clear')
+        if nome == "SAIR":
+            break
+        
+        if "." in nome:
+            for remedio in remedios:
+                if nome == remedios[remedio]['PRODUTO']:
+                    resultados.append(remedio)
+        else:
+            for remedio in remedios:
+                if nome in remedios[remedio]['PRODUTO']:
+                    resultados.append(remedio)
+        print("\nResultados:")
+        
+        for r in resultados:
+            print("-"*130)
+            print(
+                f"\nNome: {remedios[r]['PRODUTO']}\n"
+                f"Substancia: {remedios[r]['SUBSTÂNCIA']}\n"
+                f"Apresentacao: {remedios[r]['APRESENTAÇÃO']}\n"
+                f"Tipo de Produto: {remedios[r]['TIPO DE PRODUTO (STATUS DO PRODUTO)']}\n"
+                f"PMC: {remedios[r]['PMC 17%']}\n"
+                )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nSaindo...")
